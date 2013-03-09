99668e5a956c6a8350ea7c07a8bd1278ae1b6c5a
Anniepoo <annie66us@yahoo.com>
2013-03-09 11:59:47 -0800

    Server code for the reflector

---
+++ TDV/TDVControl/TDVServer.pl
@@ -0,0 +1,35 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_parameters)).
+
+start :-
+        http_server(http_dispatch, [port(6788)]).
+
+:- http_handler('/TDV', send_data, []).
+
+:- dynamic silo_data/1.
+
+silo_data(tdv_control('0,0,0,0,0,0')).
+
+send_data(_Request) :-
+	silo_data(tdv_control(Data)),
+        format('Content-type: text/plain~n~n'),
+        format('~w', [Data]).
+
+:- http_handler('/TDVcontrol', set_data, []).
+
+set_data(Request) :-
+	http_parameters(Request,
+			[
+			 data(Data, [optional(true)])
+			]),
+	ground(Data),!,
+	retractall(silo_data(tdv_control(_))),
+	asserta(silo_data(tdv_control(Data))),
+        format('Content-type: text/plain~n~n'),
+        format('OK~n', []).
+set_data(_) :-
+        format('Content-type: text/plain~n~n'),
+        format('NODATA~n', []).
+
+
--- TDV/XnaBasics/RemoteControlListener.cs
+++ TDV/XnaBasics/RemoteControlListener.cs
@@ -13,7 +13,13 @@ namespace Microsoft.Samples.Kinect.XnaBasics
     /// </summary>
     class RemoteControlListener
     {
-        private const string URI = "http://www.obscuregame.com:5001/TDV";
+        private const string URI = "http://www.obscuregame.com:6788/TDV";
+
+        /// <summary>
+        /// Time in millisec between server requests to get the control data
+
+        /// </summary>
+        private const int POLLRATE = 500;
 
         private string[] args = null;
 
@@ -23,6 +29,16 @@ namespace Microsoft.Samples.Kinect.XnaBasics
         {
             Thread t = new Thread(update);          // Kick off a new thread
             t.Start();                               // running WriteY()
+        }
+
+        public int IntArg(int index)
+        {
+            return int.Parse(args[index]);
+        }
+
+        public string StringArg(int index)
+        {
+            return args[index];
         }
 
         void update()
