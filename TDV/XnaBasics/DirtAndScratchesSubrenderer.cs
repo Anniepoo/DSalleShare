1c7bc01b1af78e1baee590efe7f68f3c17ca031c
Anniepoo <annie66us@yahoo.com>
2013-03-22 14:54:07 -0700

    Improved 8mm look

--- TDV/XnaBasics/DirtAndScratchesSubrenderer.cs
+++ TDV/XnaBasics/DirtAndScratchesSubrenderer.cs
@@ -11,6 +11,9 @@ namespace Microsoft.Samples.Kinect.XnaBasics
     {
         private static Texture2D colorBlow;
         private static Texture2D vignette;
+        private static Texture2D vignette2;
+        private static Texture2D current_vignette;
+
         private static Texture2D dirt;
         private static Texture2D frame;
         private static Color blowColor = new Color(1.0f, 1.0f, 1.0f, 0.37f);
@@ -22,6 +25,7 @@ namespace Microsoft.Samples.Kinect.XnaBasics
 
         private static int shakeOffset = 0;
         private static Point dirtOffset = new Point();
+        private const int BULB_DIVISOR = 34;
 
         public static int Shake
         {
@@ -40,16 +44,26 @@ namespace Microsoft.Samples.Kinect.XnaBasics
             Rectangle dirtSource = new Rectangle(0, 0, TDVBasicGame.Width, TDVBasicGame.Height);
             dirtSource.Offset(dirtOffset);
             ssb.Draw(dirt, screen, dirtSource, Color.White);
-            ssb.Draw(vignette, screen, vignette.Bounds, Color.White);
+            ssb.Draw(current_vignette, screen, vignette2.Bounds, Color.White);
             ssb.Draw(frame, screen, frame.Bounds, Color.White);
 
             frameNum++;
-            if (frameNum > FRAME_DIVISOR)
+            if (frameNum % FRAME_DIVISOR == 0)
             {
                 shakeOffset = rnd.Next(6) - 6;
                 dirtOffset.X = rnd.Next(dirt.Width - TDVBasicGame.Width);
                 dirtOffset.Y = rnd.Next(dirt.Height - TDVBasicGame.Height);
-                frameNum = 0;
+                if (frameNum % BULB_DIVISOR == 0)
+                {
+                    if (rnd.Next(10) == 1)
+                    {
+                        current_vignette = vignette;
+                    }
+                    else
+                    {
+                        current_vignette = vignette2;
+                    }
+                }
             }
         }
 
@@ -57,6 +71,8 @@ namespace Microsoft.Samples.Kinect.XnaBasics
         {
             frame = game.Content.Load<Texture2D>("filmframe");
             vignette = game.Content.Load<Texture2D>("vignette");
+            vignette2 = game.Content.Load<Texture2D>("vignette2");
+            current_vignette = vignette;
             dirt = game.Content.Load<Texture2D>("dirt");
         }
 
